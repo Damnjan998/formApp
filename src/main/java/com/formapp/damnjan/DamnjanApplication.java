16a8fb103af2e9d302c38a64d6431a68810b5ec5
Damnjan998 <damnjana1998@gmail.com>
2024-03-06 14:21:58 +0100

    Initialized project

---
+++ src/main/java/com/formapp/damnjan/DamnjanApplication.java
@@ -0,0 +1,13 @@
+package com.formapp.damnjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamnjanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamnjanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formapp/damnjan/DamnjanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formapp.damnjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamnjanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
